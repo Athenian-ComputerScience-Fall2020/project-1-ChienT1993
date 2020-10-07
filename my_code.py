3fb29f5d2c233a450d27c900a772abecdbfd61c7
ChienT1993 <21ctran@athenian.org>
2020-10-07 10:52:19 +0000

    unfinished

--- my_code.py
+++ my_code.py
@@ -1,7 +1,13 @@
-# Collaborators (including web sites where you got help: (enter none if you didn't need help)
+# Collaborators (including web sites where you got help: https://www.w3schools.com/python/default.asp
 #  
-
-
+print ("Android: Hello there stranger, my name is android and I'm your personal adviser on Mars.")
+print ("Android: So, what is your name?")
+name = input("Your name: ")
+print ("Android: It's a pleasure to meet you", name)
+print ("Android: On Mars, foods and water are limited, so we need to plant fruits and search for water.")
+print ("Android: Now, let me pick out 3 plants based on 3 of your number inputs.")
+plant = ["avocado", "banana", "grapefruit", "jackfruit", "kiwi", "lychee", "mango", "papaya", "peaches", "strawberry", "watermelon"]
+#3 input for random fruits according to the numbers
 
 
 
