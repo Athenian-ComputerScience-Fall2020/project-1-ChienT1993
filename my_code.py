38046de13a3dc8d200d6510c079fa18de410d92c
ChienT1993 <21ctran@athenian.org>
2020-10-13 15:50:32 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -1,22 +1,22 @@
-# Collaborators (including web sites where you got help: https://www.w3schools.com/python/default.asp
+# Collaborators (including web sites where you got help: https://www.w3schools.com/python/default.asp, https://stackoverflow.com/questions/19782075/how-to-stop-terminate-a-python-script-from-running#:~:text=To%20stop%20a%20running%20program,want%20to%20terminate%20the%20program.&text=Ctrl%20%2B%20Z%20should%20do%20it,caught%20in%20the%20python%20shell.
 #  
-
+import sys
 print ("Android: Hello there stranger, my name is Android and I'm your personal adviser on Mars.")
 print ("Android: So, what is your name?")
 name = input("Your name: ")
 print ("Android: It's a pleasure to meet you", name)
 print ("Android: On Mars, foods and water are limited, so we need to plant fruits and search for water.")
 print ("Android: Now, let me pick out 3 plants based on 3 of your number inputs.")
-plant_list = ["avocado", "banana", "grapefruit", "jackfruit", "kiwi", "lychee", "mango", "papaya", "peaches", "strawberry", "watermelon"]
+plant_list = ["avocados", "bananas", "grapefruits", "jackfruits", "kiwis", "lychees", "mangoes", "papayas", "peaches", "strawberries", "watermelons"]
 try:
-    fruit_1 = int(input("Your first number (from -11 to 10): "))
-    fruit_2 = int(input("Your second number (from -11 to 10): "))
-    fruit_3 = int(input("Your third number (from -11 to 10): "))
+    fruit_1 = int(input("Your first number (from 0 to 10): "))
+    fruit_2 = int(input("Your second number (from 0 to 10): "))
+    fruit_3 = int(input("Your third number (from 0 to 10): "))
     while fruit_1 > 0 or fruit_1 < 10 or fruit_2 > 0 or fruit_2 < 10 or fruit_3 > 0 or fruit_3 < 10: #need to look back at this later
         print ("Based on these number, I suggest you to plant " + plant_list[fruit_1] + ", " + plant_list[fruit_2] + ", and " + plant_list[fruit_3])
         break
 except:
-    print ("Please type a number in the range of 0 to 10")
+    print ("You are automatically assigned with avocados, bananas and watermelons")
 
 print ("Android: Great, now all we need is water for the plant.")
 print ("Android: Since we are on Mars and we can easily get lost, I suggest we either go straight North, East, West, or South.")
@@ -24,13 +24,13 @@ print ("Android: Since we are on Mars and we can easily get lost, I suggest we e
 def RPS(): # I will assume that these monsters don't have fingers
     user = int(input("Type 1 for rock, 2 for paper, and 3 for scissors: "))
     if user == 1:
-        print ("It is a tie, play again.")
+        print ("It's a tie, play again.")
         RPS()
     elif user == 2:
-        print ("You won, the game shall continue.")
+        print ("You won.")
     elif user == 3:
         print ("The monster has defeated you. Game over.")
-        #break# need a break here
+        sys.exit()
 
 def North():
     print ("There is nothing here other than Mars cactus.")
@@ -39,13 +39,43 @@ def North():
 def East():
     print ("Android: Oh no, there's a monster ahead, prepare for a battle of rock, paper, scissors!")
     RPS()
+    print ("Android: Yay, we did it, we manage to defeat the monster!")
+    print ("Android: It turns out, the monster we defeat earlier was guarding this lake.")
+    print ("Android: But, we only have 100 liters of water, how do you want to distribute it?")
+    print ("Water have 2 main purposes, to drink (use) and to water the plant.")
+    liter = int(input("How much water will you save for the purpose of drinking (use)?: "))
+    for liter in range (0, 80):
+        print ("You die because you run out of water. Game over.")
+        break
+    else:
+        print ("You survived a month on Mars but your plants run out of water and die out. You live for another 2 weeks then die.")
+        print ("Game over.")
 
 def West():
     print ("Android: Hey, I found a shiny item. It looks like a diamond, but is not made out of Carbon.")
-    #leave the planet or not
+    print ("Android: Do you want to leave this planet with this special artifact?")
+    decision = input("Type 'leave' to leave this planet or 'stay' to stay here: ")
+    if decision == "leave":
+        print ("You decided to leave Mars, but you run out of food and water supplies on your way back to Earth, you die.")
+        print ("Game over.")
+    if decision == "stay":
+        print ("Android: Look, there is a monster heading toward us. Quick ready your fingers, we are going to have a rock, paper, scissors game.")
+        RPS()
+        print ("Moster: foiuaefpijfuiaefjiogegdiojefoingwebiueioudsvnioeiubhrpimaweaoihhookerwiubrgwoifuifeq!")
+        print ("Android: The moster acknowledge your strength and decided to crowned you as the king/queen of Mars!")
+        print ("You live a happy life on Mars, marry a Mars-ian citizen and give birth to a son. Congratulation")
 
 def South():
     print ("Android: Oh look, there's a lake over there.")
     print ("Android: With this amount of water, we can last at least 10 more years on Mars.")
     print ("Congratulation, you survive.")
 
+direction = str(input("Which direction will you take?(North, East, West, South): "))
+if direction == "North":
+    North()
+if direction == "East":
+    East()
+if direction == "West":
+    West()
+if direction == "South":
+    South()
