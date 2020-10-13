5a7ebe39d0d4e7dd6ac8dc768f639322117df3e3
ChienT1993 <21ctran@athenian.org>
2020-10-13 12:29:37 +0000

    unfinish

--- my_code.py
+++ my_code.py
@@ -1,6 +1,5 @@
 # Collaborators (including web sites where you got help: https://www.w3schools.com/python/default.asp
 #  
-import random
 
 print ("Android: Hello there stranger, my name is Android and I'm your personal adviser on Mars.")
 print ("Android: So, what is your name?")
@@ -13,17 +12,25 @@ try:
     fruit_1 = int(input("Your first number (from -11 to 10): "))
     fruit_2 = int(input("Your second number (from -11 to 10): "))
     fruit_3 = int(input("Your third number (from -11 to 10): "))
-    while fruit_1 > -11 or fruit_1 < 10 or fruit_2 > -11 or fruit_2 < 10 or fruit_3 > -11 or fruit_3 < 10: #need to look back at this later
+    while fruit_1 > 0 or fruit_1 < 10 or fruit_2 > 0 or fruit_2 < 10 or fruit_3 > 0 or fruit_3 < 10: #need to look back at this later
         print ("Based on these number, I suggest you to plant " + plant_list[fruit_1] + ", " + plant_list[fruit_2] + ", and " + plant_list[fruit_3])
         break
 except:
-    print ("Please type a number in the range of -11 to 10")
+    print ("Please type a number in the range of 0 to 10")
 
 print ("Android: Great, now all we need is water for the plant.")
-print ("Android: Since we are on Mars and we can easily get lost, I suggest we either go straight North, East, West, or South")
+print ("Android: Since we are on Mars and we can easily get lost, I suggest we either go straight North, East, West, or South.")
 
-def RPS():#random number (1,2,3) for rock paper scissors
-    print ("Type 1 for rock, 2 for paper, and 3 for scissors: ")
+def RPS(): # I will assume that these monsters don't have fingers
+    user = int(input("Type 1 for rock, 2 for paper, and 3 for scissors: "))
+    if user == 1:
+        print ("It is a tie, play again.")
+        RPS()
+    elif user == 2:
+        print ("You won, the game shall continue.")
+    elif user == 3:
+        print ("The monster has defeated you. Game over.")
+        #break# need a break here
 
 def North():
     print ("There is nothing here other than Mars cactus.")
@@ -31,13 +38,14 @@ def North():
 
 def East():
     print ("Android: Oh no, there's a monster ahead, prepare for a battle of rock, paper, scissors!")
-    #RPS
+    RPS()
 
 def West():
-    print ("Android: Hey, I found a shiny item. It looks like a diamond, but is not made out of CO2")
+    print ("Android: Hey, I found a shiny item. It looks like a diamond, but is not made out of Carbon.")
     #leave the planet or not
 
 def South():
     print ("Android: Oh look, there's a lake over there.")
     print ("Android: With this amount of water, we can last at least 10 more years on Mars.")
     print ("Congratulation, you survive.")
+
